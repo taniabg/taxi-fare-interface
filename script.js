922097b911a5d6b331b96e354bb65e84f4e4f8cd
taniabg <65242509+taniabg@users.noreply.github.com>
2024-08-23 12:47:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const centralCoordinates = [-74.00597, 40.71427]; // starting position [lng, lat
 if (window.location.href.includes('https://taxifare.lewagon.com')) {
   taxiFareApiUrl = 'https://taxifare.lewagon.ai/predict';
 }
-
+const taxiFareApiUrl = 'https://taxifare.lewagon.ai/predict';
 mapboxgl.accessToken = 'pk.eyJ1Ijoia3Jva3JvYiIsImEiOiJja2YzcmcyNDkwNXVpMnRtZGwxb2MzNWtvIn0.69leM_6Roh26Ju7Lqb2pwQ';
 
 const displayMap = (start, stop) => {
